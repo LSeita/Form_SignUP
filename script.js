e5f96905e85e7422d2514db3e87921fdfbcbb242
LSeita <seita.lucca@gmail.com>
2023-04-28 16:55:50 -0300

    Adiciona Javascript e comparação de senhas

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const password = document.getElementById("password");
 const confirm_pw = document.getElementById("confirm_password");
+const message = document.getElementsByClassName("error-msg");
+const button = document.querySelector("button");
+const form = document.querySelector("form");
 
-password.addEventListener("keyup", checkPW())
-confirm_pw.addEventListener("keyup", checkPW())
+for(let i=0; i<message.length;i++){
+    message[i].classList.add("error-visible");
+}
 
-
-function checkPW(){
-    if(password!=confirm_pw){
-        password.classList.add("error");
-        confirm_pw.classList.add("error");
+button.addEventListener("click", (event) => {
+    if(password.value!=confirm_pw.value){
+        event.preventDefault();
+        password.classList.add("error")
+        confirm_pw.classList.add("error")
+        for(let i=0; i<message.length;i++){
+            message[i].classList.remove("error-visible");
+        }
     }
     else if(password.classList.contains("error") || confirm_pw.classList.contains("error")){
-        password.classList.remove("error");
-        confirm_pw.classList.remove("error");
+        password.classList.remove("error")
+        confirm_pw.classList.remove("error")
+        for(let i=0; i<message.length;i++){
+            message[i].classList.add("error-visible");
+        }
     }
-}
+});
